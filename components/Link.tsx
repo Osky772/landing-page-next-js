ae149060e949a9e34bb66956b948f4217e7e85f8
Oskar Kowalów <kowalow.oskar@gmail.com>
2021-05-03 17:16:32 +0200

    refactor + change styling of Link

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
-import Link from 'next/link'
+import NextLink from 'next/link'
 
 const MenuLink = styled.a`
     list-style-type: none;
     margin-right: 55px;
-    color: ${props => props.theme.colors.links.primary};
+    color: ${props => props.theme.colors.secondary};
     cursor: pointer;
+    display: flex;
+    align-items: center;
 `
 
 type MenuItemT = {
@@ -14,10 +16,10 @@ type MenuItemT = {
     href: string;
 }
 
-function MenuItem({ children, href }: MenuItemT) {
+function Link({ children, href }: MenuItemT) {
     return (
-        <Link href={href}><MenuLink>{ children }</MenuLink></Link>
+        <NextLink href={href}><MenuLink>{ children }</MenuLink></NextLink>
     )
 }
 
-export default MenuItem
+export default Link
